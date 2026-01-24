989ea6a656cba733be34cadd1d307e7021db94f4
AnvarMirsoatov <anvarmirsaatov6358@gmail.com>
2026-01-24 12:33:46 +0500

    bug fix

--- src/app/[locale]/super-admin/penalties/[slug]/page.tsx
+++ src/app/[locale]/super-admin/penalties/[slug]/page.tsx
@@ -1,5 +1,13 @@
 import PenaltiesClient from "../PenaltiesClient";
 
-export default function Page({ params }: { params: { slug: string } }) {
+// export default function Page({ params }: { params: { slug: string } }) {
+type PageProps = {
+  params: {
+    slug: string;
+  };
+};
+
+export default function Page({ params }: PageProps) {
+  const { slug } = params;
   return <PenaltiesClient slug={params.slug} />;
 }
